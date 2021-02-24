a0cf249f52bc82941912e2176b4e94d799bd6904
Elevate DeFi <77861799+elevatedefi@users.noreply.github.com>
2021-02-24 11:19:17 -0500

    Updates from Certik audit

--- ReflectiveTreasury.sol
+++ ReflectiveTreasury.sol
@@ -5,11 +5,12 @@ pragma solidity >=0.6.0 <0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @dev A token holder contract that will allow a beneficiary to extract only interest eanred on the principle deposited.
  */
-contract ReflectiveTreasury is Ownable {
+contract ReflectiveTreasury is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
@@ -30,21 +31,21 @@ contract ReflectiveTreasury is Ownable {
     /**
      * @return the token being held.
      */
-    function token() public view returns (IERC20) {
+    function token() external view returns (IERC20) {
         return _token;
     }
 
     /**
      * @return the beneficiary of the tokens.
      */
-    function beneficiary() public view returns (address) {
+    function beneficiary() external view returns (address) {
         return _beneficiary;
     }
 
     /**
      * @return the principal that has been been added to the contract.  This can never be withdrawn.
      */
-    function principle() public view returns (uint256) {
+    function principle() external view returns (uint256) {
         return _principle;
     }
 
@@ -58,7 +59,7 @@ contract ReflectiveTreasury is Ownable {
     /**
      * @notice Deposit funds to the treasury.
      */
-    function deposit(uint256 amount) external {
+    function deposit(uint256 amount) external nonReentrant {
         _token.safeTransferFrom(msg.sender, address(this), amount); 
         amount = _applyFee(amount);
         _principle = _principle.add(amount);
@@ -77,7 +78,7 @@ contract ReflectiveTreasury is Ownable {
     /**
      * @notice Withdraw entire balance to the owner.
      */
-    function withdraw() external onlyOwner() {
+    function withdraw() external onlyOwner nonReentrant {
         uint256 amount = _token.balanceOf(address(this));
         require(amount > 0, "Treasury: no balance to withdraw");
         _token.safeTransfer(msg.sender, amount);
@@ -87,14 +88,14 @@ contract ReflectiveTreasury is Ownable {
     /**
      * @notice Sets a new beneficiary.
      */
-    function setBeneficiary(address newBeneficiary) external onlyOwner() {
+    function setBeneficiary(address newBeneficiary) external onlyOwner {
         _beneficiary = newBeneficiary;
     }
 
     /**
      * @notice Transfers tokens to beneficiary.
      */
-    function release() public {
+    function release() external {
         uint256 amount = fundsAvailable();
         require(amount > 0, 'Treasury: no funds to release');
         _token.safeTransfer(_beneficiary, amount);
